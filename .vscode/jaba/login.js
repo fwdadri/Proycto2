74425b6864a237de6e11ade8246b3375ac38b5f8
fwdadri <areyes@fwdcostarica.com>
2024-05-09 13:06:45 -0600

    funciones login y register

--- .vscode/jaba/login.js
+++ .vscode/jaba/login.js
@@ -6,30 +6,28 @@ let password = document.getElementById("password");
 let boton = document.getElementById("botonLogin");
 
 
-function boton() {
-
+boton.addEventListener("click", function( ) {
     if (username.value != "" && password.value !="" ) {
-    
-      registro = JSON.parse(localStorage.getItem("usuario"))
-    
-      console.log(usuario)
-
-      let encontrado = registro.find(usuario => usuario.correo == username.value && usuario.password == password.value)
-        if (encontrado) {
-            localStorage.setItem("usuario",JSON.stringify(encontrado))
-            alert ("registro encontrado")
-            window.location.href= "principal.html"
-        
-        }else{
-                alert ("no pude entrar")
-        }
-
-
-    }else{
-        alert ("campos incompletros")
-    }
+        //obtener una sola parte                   //nombre del key
+         registro = JSON.parse(localStorage.getItem("usuario"))
+ 
+        //esos dos tienen el registro
    
+        // el find es cmo un for que recorre una lista o array
+          let encontrado = registro.find(key => key.gmail == username.value && key.password == password.value)
+                                      //es un key, puede tener cualquier key como index que recorre y es el que encuentra lo que queremos
+           if (encontrado) {
+               localStorage.setItem("usuario",JSON.stringify(encontrado))
+               alert ("registro encontrado")
+               window.location.href= "principal.html"
            
-}  
-
+           }else{
+                   alert ("no puede entrar");
+           }
+   
+   
+       }else{
+           alert ("campos incompletros")
+       }
+}) 
 
--- .vscode/jaba/principal.js
+++ .vscode/jaba/principal.js
@@ -1,16 +1,39 @@
 const user = JSON.parse(localStorage.getItem('login')) || false
-if (!user) {
+if (!user) { //para aque no entre si pone la direccion de la pagina en el navegador
     window.location.href = 'login.html'
 }
 
 
 const salir = document.getElementById("salir")
-
+///boton d salir
 salir.addEventListener('click', function() {
 
     alert('bye')
 
     localStorage.removeItem('principal.html')
 
-    window.location.href = "login.html"
+    window.location.href = "login.html" //devolever al login
 })
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+
+let guardar = document.getElementById("btnGuardar");
+
+
+guardar.addEventListener('click', function() {
+    alert("holi")
+    let nombre = document.getElementById("nombre")
+    let src = document.getElementById("src")
+    
+   //trtando de generar filas
+    document.getElementById("cantidad").insertRow(-1).innerHTML = '<td></td><td></td><td></td><td></td><td></td><td></td>';
+
+    document.getElementById("nombreAr").innerHTML = nombre.value
+
+    document.getElementById("nombreSrc").innerHTML = src.value
+})
+
+
