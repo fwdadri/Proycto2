cd23cf3e1aae42ae872ddb86e4d2969f7067061c
fwdadri <areyes@fwdcostarica.com>
2024-05-13 16:19:04 -0600

    cambios

--- .vscode/jaba/login.js
+++ .vscode/jaba/login.js
@@ -2,34 +2,31 @@
 
 let username = document.getElementById("username");
 let password = document.getElementById("password");
-
 let boton = document.getElementById("botonLogin");
 
 
 boton.addEventListener("click", function( ) {
     if (username.value != "" && password.value !="" ) {
-        //obtener una sola parte                   //nombre del key
-         registro = JSON.parse(localStorage.getItem("crear"))
- 
+        //el parse es obtener una sola parte                   
+        registro = JSON.parse(localStorage.getItem("crear"))//nombre del key
         //esos dos tienen el registro
-   
         console.log(username.value + password.value)
         console.log(registro)
-        // el find es cmo un for que recorre una lista o array
-          let encontrado = registro.find(key => key.gmail == username.value && key.password == password.value)
-                                      //es un key, puede tener cualquier key como index que recorre y es el que encuentra lo que queremos
-           if (encontrado) {
 
-               localStorage.setItem("usuario",JSON.stringify(encontrado))
-               alert ("bienvenido");
-               window.location.href = "principal.html"
+        let encontrado = registro.find(key => key.gmail == username.value && key.password == password.value) 
+//es un key, puede tener cualquier key como index que recorre y es el que encuentra lo que queremos
+     if (encontrado) {              // el find es cmo un for que recorre una lista o array
+
+      localStorage.setItem("usuario",JSON.stringify(encontrado))
+       alert ("bienvenido");
+       window.location.href = "principal.html"
            
-           }else{
-                   alert ("contraseña o correo incorrecto");
-           }
-   
-   
-       }else{
-           alert ("campos incompletos")
-       }
-}) 
+        }else{
+         alert ("contraseña o correo incorrecto");
+        }
+      
+    }else{ // un if solo puede tener un else
+        alert ("campos incompletos");
+    }
+})
+
